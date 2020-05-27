e4a422c34f82e7613b94bb01672aae6a260f80ae
Javier <javier.martinezf@usm.cl>
2020-05-27 15:46:35 -0400

    puse a andar una pagina web con python

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-import turtle
-
-t=turtle.Turtle()
-input()
+from flask import Flask
+app= Flask(__name__)
+@app.route('/')
+def index():
+    return "Hola mundo"
+if __name__=="__main__":
+    app.run()
